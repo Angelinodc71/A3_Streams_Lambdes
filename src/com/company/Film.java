448ce73f726e2a1277628c4350ccd7157bb05f7d
angelinodc71 <adec58@elpuig.xeill.nety>
2020-01-10 19:19:08 +0100

    Initial commit

---
+++ src/com/company/Film.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Film {
+    String idfilm;
+    String prioritat;
+    String titol;
+    String situacio;
+    String any;
+    String cartell;
+    String original;
+    String direccio;
+    String interprets;
+    String sinopsi;
+    String versio;
+    String idioma_x0020_original;
+    String qualificacio;
+    String trailer;
+    String web;
+    String estrena;
+
+    public Film(String idfilm, String prioritat, String titol, String situacio, String any, String cartell, String original, String direccio, String interprets, String sinopsi, String versio, String idioma_x0020_original, String qualificacio, String trailer, String web, String estrena) {
+        this.idfilm = idfilm;
+        this.prioritat = prioritat;
+        this.titol = titol;
+        this.situacio = situacio;
+        this.any = any;
+        this.cartell = cartell;
+        this.original = original;
+        this.direccio = direccio;
+        this.interprets = interprets;
+        this.sinopsi = sinopsi;
+        this.versio = versio;
+        this.idioma_x0020_original = idioma_x0020_original;
+        this.qualificacio = qualificacio;
+        this.trailer = trailer;
+        this.web = web;
+        this.estrena = estrena;
+    }
+
+
+
+}
